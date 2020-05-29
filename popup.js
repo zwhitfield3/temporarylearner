13adfc6df8d866d75539d97986f97d73fe78e2c9
Zane Whitfield <zwhitfield@salesforce.com>
2020-05-28 18:45:40 -0700

    Grab correct id from popup.html

--- popup.js
+++ popup.js
@@ -1,11 +1,20 @@
-document.addEventListener('DOMContentLoaded', function (event) {
-    const radio = document.getElementById("radioButton1")
-    radio.addEventListener('change', changeHandler)
-    // console.log('something', event)
+document.addEventListener('DOMContentLoaded', function () {
+    const newInfoAvailableRadio = document.getElementById("subscriptionCadence-newInfoAvailable")
+    const onceAWeekRadio = document.getElementById("subscriptionCadence-onceAWeek")
+    // radio.addEventListener('change', changeHandler)
+    newInfoAvailableRadio.addEventListener('change', changeHandler)
+    onceAWeekRadio.addEventListener('change', changeHandler)
 
 });
-function changeHandler() {
+function changeHandler(event) {
+    if (event.target.id === 'subscriptionCadence-newInfoAvailable') {
+        console.log('WE MADE IT!')
+    }
+
+
     console.log('a radio was clicked!')
+
+
 
     // if (radioButton1.checked) {
     //     radioButton1.checked = false
