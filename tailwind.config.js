5c5263f8f16d495390d50cd61189c3373e9e8086
Zane Whitfield <whitfield.zane@gmail.com>
2020-05-30 15:44:21 -0700

    Extend height of chrome extension popup

--- tailwind.config.js
+++ tailwind.config.js
@@ -288,7 +288,7 @@ module.exports = {
       ...theme("spacing"),
       full: "100%",
       screen: "100vh",
-      chromeExtensionHeight: "500px",
+      chromeExtensionHeight: "550px",
     }),
     hr: {
       border: "0",
