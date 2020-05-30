032d971f340d4a48305bc0dcac48e70967e825e9
Zane Whitfield <whitfield.zane@gmail.com>
2020-05-29 22:43:40 -0700

    Fixed merge conflicts and got selection dropdown working

--- popup.js
+++ popup.js
@@ -4,13 +4,40 @@ document.addEventListener("DOMContentLoaded", function () {
   );
   subscribeViaUserButton.addEventListener("change", changeHandler);
 });
+
+// function generalElementGrabber() {
+//     let generalElement = document
+// }
+
 function changeHandler(event) {
   console.log("event", event);
   //   debugger;
+  const listElement = document.getElementById("userSelection");
   const eventWithTarget = event.target;
   if (eventWithTarget.id === "subscriptionType-byUser") {
     if (eventWithTarget.checked === true) {
-      console.log("WE MADE IT");
+      //   console.log("WE MADE IT");
+      addOptionElement(eventWithTarget.id);
+      toggleHide(listElement.id);
     }
+  }
+}
+
+function addOptionElement(elementID) {
+  const userOption = document.createElement("option");
+  const listElement = document.getElementById("userSelection");
+
+  userOption.value = "enaz321";
+  userOption.text = "Enaz321";
+
+  listElement.appendChild(userOption);
+}
+
+function toggleHide(elementID) {
+  const elementToToggle = document.getElementById(elementID);
+  if (elementToToggle.hidden === false) {
+    elementToToggle.hidden = true;
+  } else {
+    elementToToggle.hidden = false;
   }
 }
