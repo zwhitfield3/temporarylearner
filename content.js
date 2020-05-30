13c4dc54ac1d0228f28589f5a620ef044c698d68
Zane Whitfield <whitfield.zane@gmail.com>
2020-05-30 14:22:42 -0700

    IDK stuff is working now

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
-console.log('chrome extension we are a go!')
+console.log("chrome extension we are a go!");
 
-chrome.runtime.onMessage.addListener(gotMessage)
+chrome.runtime.onMessage.addListener(gotMessage);
 
 //message is the message from background.js
-function gotMessage (message, sender, sendResponse) {
-  console.log(message.txt)
+function gotMessage(message, sender, sendResponse) {
+  console.log(message.txt);
 }
