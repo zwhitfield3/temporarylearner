9dee95e5be85c10cda96c2137a5d92971d4a482e
Zane Whitfield <whitfield.zane@gmail.com>
2020-05-29 21:53:56 -0700

    Add logic to changeHandler for subscription checkbox

--- popup.js
+++ popup.js
@@ -1,32 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const newInfoAvailableRadio = document.getElementById("subscriptionCadence-newInfoAvailable")
-    const onceAWeekRadio = document.getElementById("subscriptionCadence-onceAWeek")
-    // radio.addEventListener('change', changeHandler)
-    newInfoAvailableRadio.addEventListener('change', changeHandler)
-    onceAWeekRadio.addEventListener('change', changeHandler)
-
+document.addEventListener("DOMContentLoaded", function () {
+  const subscribeViaUserButton = document.getElementById(
+    "subscriptionType-byUser"
+  );
+  subscribeViaUserButton.addEventListener("change", changeHandler);
 });
 function changeHandler(event) {
-    // debugger
-    if (event.target.id === 'subscriptionCadence-newInfoAvailable') {
-        console.log('WE MADE IT')
-        event.target.checked = 'checked'
-        console.log('checked status', event.target.checked)
-    } else if (event.target.id === 'subscriptionCadence-onceAWeek') {
-        console.log('WE MADE IT')
-        event.target.checked = 'checked'
-        console.log('checked status', event.target.checked)
+  console.log("event", event);
+  //   debugger;
+  const eventWithTarget = event.target;
+  if (eventWithTarget.id === "subscriptionType-byUser") {
+    if (eventWithTarget.checked === true) {
+      console.log("WE MADE IT");
     }
-
-
-    console.log('a radio was clicked!')
-
-
-
-    // if (radioButton1.checked) {
-    //     radioButton1.checked = false
-    // }
-    // else {
-    //     radioButton1.checked = true
-    // }
+  }
 }
