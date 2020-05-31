cd9ff02dec0d5aa137ac3dc796c8b1da7beba7e6
Zane Whitfield <whitfield.zane@gmail.com>
2020-05-30 21:48:18 -0700

    WIP add working navbar logic

--- popup.js
+++ popup.js
@@ -6,6 +6,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const submitButton = document.getElementById("submit");
   submitButton.addEventListener("click", saveInfo);
+
+  const navbar = document.getElementById("navbar");
+  navbar.addEventListener("click", toggleNavBar);
 });
 
 function generalElementGrabber(elementID) {
@@ -13,6 +16,22 @@ function generalElementGrabber(elementID) {
 }
 
 let userData = [];
+
+function toggleNavBar() {
+  const createButton = generalElementGrabber("create");
+  const subscriptionsButton = generalElementGrabber("subscriptions");
+  const settingsButton = generalElementGrabber("settings");
+
+  const currentActiveElement = document.activeElement.id;
+
+  if (currentActiveElement === "create") {
+    createButton.classList.appendChild("bg-blue-500");
+  } else if (currentActiveElement === "subscriptions") {
+    subscriptionsButton.classList.add("bg-blue-500");
+  } else if (currentActiveElement === "settings") {
+    settingsButton.classList.add("bg-blue-500");
+  }
+}
 
 function saveInfo() {
   let subscriptionData = {};
