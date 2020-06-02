f8abe2a0bb02c10242370597dff6b0aadded9463
Zane Whitfield <whitfield.zane@gmail.com>
2020-06-02 08:36:03 -0700

    WIP toggle share feedback button

--- app/js/settings.js
+++ app/js/settings.js
@@ -2,6 +2,7 @@ import * as data from "./data.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   loadUserData();
+  userSelection();
 
   // submitInfo();
 });
@@ -18,6 +19,11 @@ function loadUserData() {
 function submitInfo() {
   const submitButton = document.getElementById("submit");
   submitButton.addEventListener("click", saveInfo);
+}
+
+function userSelection() {
+  const shareFeedbackButton = document.getElementById("shareFeedback");
+  shareFeedbackButton.addEventListener("click", toggleHide);
 }
 
 function saveInfo() {
@@ -54,30 +60,16 @@ function saveInfo() {
   console.log("subscriptionData", appData);
 }
 
-//   function addOptionElement(elementID) {
-//     const elementToManipulate = document.getElementById(elementID);
-//     const listElement = document.getElementById("userSelection");
-//     const userOption = document.createElement("option");
+function toggleHide() {
+  const buttonElement = document.getElementById("shareFeedback");
+  const elementToToggle = document.getElementById("feedbackForum");
 
-//     if (elementToManipulate.checked === true) {
-//       userOption.value = "enaz321";
-//       userOption.text = "Enaz321";
-//       userOption.id = "1";
-
-//       listElement.appendChild(userOption);
-//     } else {
-//       listElement.removeChild(document.getElementById("1"));
-//     }
-//   }
-
-//   function toggleHide(listElementID, checkboxElementID) {
-//     //   debugger;
-//     const checkboxElement = document.getElementById(checkboxElementID);
-//     const elementToToggle = document.getElementById(listElementID);
-
-//     if (checkboxElement.checked) {
-//       elementToToggle.hidden = false;
-//     } else {
-//       elementToToggle.hidden = true;
-//     }
-//   }
+  buttonElement.addEventListener("click", function () {
+    // debugger;
+    if (elementToToggle.hidden) {
+      elementToToggle.hidden = false;
+    } else {
+      elementToToggle.hidden = true;
+    }
+  });
+}
